6eae183f745c2441bab0fdf81b440a415541e99b
Patrick-Lenihan <61756898+Patrick-Lenihan@users.noreply.github.com>
2021-04-23 00:58:31 +0100

    Add files via upload

--- ca2/game.js
+++ ca2/game.js
@@ -36,7 +36,7 @@ let background = [
     [45,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,45],
     [ 45,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47,47, 45],
     [ 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45]];
-
+// setting up main player object
 let player = {
     type:"player",
     xPos:100,
@@ -52,17 +52,34 @@ let player = {
     direction:null,
     time_until_rearm:180,
 };
+// all images must be loaded into there own vars so the client has them in good time durring the game
 let playerImage = new Image();
 let backgroundImage = new Image();
 let enemyImage = new Image();
 let disarmImage = new Image();
 let chaserImage = new Image();
+let playerImageLeft = new Image();
+let playerImageRight = new Image();
+let playerImageUp = new Image();
+let playerImageDown = new Image();
+let playerImageAttackLeft = new Image();
+let playerImageAttackRight = new Image();
+let playerImageAttackUp = new Image();
+let playerImageAttackDown = new Image();
+let playerImageDefend = new Image();
+let playerImageHitLeft = new Image();
+let playerImageHitRight = new Image();
+let playerImageHitUp = new Image();
+let playerImageHitDown = new Image();
+let playerImageCur = 0;
+let display_level = document.querySelector("#level");
+let frames_since_start = 0;
 
-
+// objects will be a list of all physical objects displayed on the screen (apart from background tileset and spells)
 let objects = [player];
+// this populates objects with all the enemys we need for level 0
 populate_objects(1,0)
-//let enemys = [enemy];
-//let chasers = [chaser];
+// fire_spells will store all the fire spell objects used for killing enemys 
 let fire_spells = [];
 document.addEventListener("DOMContentLoaded", init, false);
 
@@ -70,6 +87,19 @@ function init() {
     canvas = document.querySelector("canvas");
     context = canvas.getContext("2d");
     playerImage.src = "Wizard_Pack/Idle.png";
+    playerImageLeft.src = "Wizard_Pack/Run_Left.png";
+    playerImageUp.src = "Wizard_Pack/Run_up.png";
+    playerImageRight.src = "Wizard_Pack/Run.png";
+    playerImageDown.src = "Wizard_Pack/Run_down.png";
+    playerImageAttackLeft.src = "Wizard_Pack/Attack2_left.png"
+    playerImageAttackRight.src = "Wizard_Pack/Attack2.png"
+    playerImageAttackUp.src = "Wizard_Pack/Attack2_up.png"
+    playerImageAttackDown.src = "Wizard_Pack/Attack2_down.png"
+    playerImageDefend.src = "Wizard_Pack/Attack1.png";
+    playerImageHitLeft.src = "Wizard_Pack/Hit.png";
+    playerImageHitRight.src = "Wizard_Pack/Hit_Right.png";
+    playerImageHitUp.src = "Wizard_Pack/Hit_up.png";
+    playerImageHitDown.src = "Wizard_Pack/Hit_down.png";
     backgroundImage.src = "Dungeon_Tileset.png";
     enemyImage.src = "death.png";
     disarmImage.src = "disarm.png";
@@ -79,14 +109,19 @@ function init() {
     draw();
 }
 function draw(){
+    console.log(player.casting);
+    //if the game is over freeze everything
     if(game_over===false){
     window.requestAnimationFrame(draw);
     }
-    //console.log(enemys.length);
+    // recording the number of frames since the start of the game 
+    //useful for when we don't want an enemy to spawn and then kill the player imediately
+    frames_since_start += 1
     controle_frame_rate();
     context.clearRect(0,0,canvas.width,canvas.height);
     display_background();
-    context.drawImage(playerImage,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    display_player();
+    // draws all instances of enemy and chaser
     for (let x = 0; x < objects.length; x += 1){
         if (!(objects[x].casting === "dead")){
             if (objects[x].type === "enemy"){
@@ -109,13 +144,13 @@ function draw(){
     move_enemy();
     move_chasers();
     if(game_over=== true){
+        // learned how to use fillText at https://www.w3schools.com/graphics/canvas_text.asp
     context.fillStyle ="red"
     context.font = "30px Arial"; 
     context.fillText("GAME OVER", 150, 150);
+    context.fillText("click c to continue", 140, 200);
     }
     check_level_up();
-    //console.log(enemy.casting);
-    //console.log(player.casting);
 
 }
 
@@ -151,6 +186,13 @@ function activate(event) {
     else if (key === "a"){
         if(player.casting === "no"){
         player.casting = "teleport";}
+    }
+    else if ((key === "c")&&(game_over = true)){
+        // if you press c you restart the game
+        game_over = false;
+        objects = [player];
+        level = 0;
+        draw();
     }
 
 }
@@ -201,9 +243,9 @@ function deactivate(event) {
     }
 }
 function move_player(){
-    //if (!(player.casting === "no")){return} 
+    // this function contains the running animation for the player and the moving logic
     if(moveLeft && (-40<player.xPos)){
-        playerImage.src = "Wizard_Pack/Run_Left.png";
+        playerImageCur= 2;
         if(player.casting==="teleport"){player.xChange -= 14;}
         player.frameY = 0;
         player.frameX = (player.frameX + 231)%1848;
@@ -213,12 +255,12 @@ function move_player(){
         if(player.casting==="teleport"){player.xChange += 14;}
         player.xChange += 1;
         player.frameY = 0;
-        playerImage.src = "Wizard_Pack/Run.png";
+        playerImageCur = 1;
         player.frameX = (player.frameX + 231)%1848;
     }
     if(moveUp && (-30<player.yPos)){
         if(player.casting==="teleport"){player.yChange -= 14;}
-        playerImage.src = "Wizard_Pack/Run_up.png";
+        playerImageCur = 3;
         player.frameX = 0;
         player.frameY = (player.frameY + 231)%1848;
         player.yChange -= 1;
@@ -226,7 +268,7 @@ function move_player(){
     }
     if(moveDown && (player.yPos<200) ){
         if(player.casting==="teleport"){player.yChange += 14;}
-        playerImage.src = "Wizard_Pack/Run_down.png";
+        playerImageCur = 4
         player.frameX = 0;
         player.frameY = (player.frameY + 231)%1848;
         player.yChange += 1;
@@ -241,6 +283,7 @@ function controle_frame_rate(){
     then = now - (elapsed % fpsInterval);
 }
 function calculate_pos(){
+    // physics
     player.xChange = player.xChange * 0.9;// friction
     player.yChange = player.yChange * 0.9;//friction
     player.xPos += player.xChange;
@@ -248,28 +291,30 @@ function calculate_pos(){
 }
 
 function cast_spell(){
+    // moves all the fire spells
     move_spells();
+    // logic for player casting disarms
     if (player.casting === "disarm"){
         if (player.direction === "left"){
-            playerImage.src = "Wizard_Pack/Attack2_left.png";
+            playerImageCur = 5;
             player.frameY = 0;
             player.frameX = (player.frameX + 231)%1848;
             cast_disarm("left");
     }
     else if (player.direction === "up"){
-            playerImage.src = "Wizard_Pack/Attack2_up.png";
+            playerImageCur = 7;
             player.frameX = 0;
             player.frameY = (player.frameY + 231)%1848;
             cast_disarm("up");
     }
     else if (player.direction === "down"){
-            playerImage.src = "Wizard_Pack/Attack2_down.png";
+            playerImageCur = 8;
             player.frameX = 0;
             player.frameY = (player.frameY + 231)%1848;
             cast_disarm("down");
     }
     else{
-    playerImage.src = "Wizard_Pack/Attack2.png";
+    playerImageCur = 6;
     player.frameY = 0;
     player.frameX = (player.frameX + 231)%1848;
     cast_disarm("right");
@@ -278,36 +323,37 @@ function cast_spell(){
 if (player.casting === "no" && player.moving === false){
     player.frameX = 0;
     player.frameY = 0;
-    playerImage.src = "Wizard_Pack/idle.png";
+    playerImageCur = 0;
 }
+// casts fire spells
 if(player.casting == "fire"){
-    // pretty ineficent way to do this if you ask me, must fix later reuses code
     if (player.direction === "left"){
-            playerImage.src = "Wizard_Pack/Attack2_left.png";
+            playerImageCur = 5;
             player.frameY = 0;
             player.frameX = (player.frameX + 231)%1848;
             cast_fire("left");
     }
     else if (player.direction === "up"){
-            playerImage.src = "Wizard_Pack/Attack2_up.png";
+            playerImageCur = 7;
             player.frameX = 0;
             player.frameY = (player.frameY + 231)%1848;
             cast_fire("up");
     }
     else if (player.direction === "down"){
-            playerImage.src = "Wizard_Pack/Attack2_down.png";
+            playerImageCur = 8;
             player.frameX = 0;
             player.frameY = (player.frameY + 231)%1848;
             cast_fire("down");
     }
     else{
-    playerImage.src = "Wizard_Pack/Attack2.png";
+    playerImageCur = 6;
     player.frameY = 0;
     player.frameX = (player.frameX + 231)%1848;
     cast_fire("right");
     }
 
 }
+// checking if we should cast a defend spell
 if(player.casting === "defend"){
     defend_player();
 }
@@ -318,50 +364,39 @@ function create_boundries(){
     if(!(((-30<player.yPos) && (player.yPos<200)))){player.yChange = 0;}
 }
 function cast_disarm(dir){
+    //checking if sombody has been hit by a dissarm
     if(dir === "left"){
-        //console.log("player",player.yPos)
-        //console.log("enemy",enemy.yPos)
         for(let x of objects){
             if ((x.xPos< player.xPos)&&(x.yPos-80 < player.yPos )){
                 if((x.yPos>player.yPos)){
                 x.casting = "disarmed";
-                //console.log("hit, sir a very palpible hit.");
             }
             }
 }
     }
 if(dir === "right"){
-        //console.log("player",player.yPos)
-        //console.log("enemy",enemy.yPos)
         for(let x of objects){
             if ((x.xPos> player.xPos)&&(x.yPos-80 < player.yPos )){
                 if((x.yPos>player.yPos)){
                 x.casting = "disarmed";
-                //console.log("hit, sir a very palpible hit.");
             }
             }
 }
     }
     if(dir === "down"){
-        //console.log("player",player.yPos)
-        //console.log("enemy",enemy.yPos)
         for(let x of objects){
             if ((x.yPos> player.yPos)&&(x.xPos-80 < player.xPos )){
                 if((x.xPos>player.xPos)){
                 x.casting = "disarmed";
-                //console.log("hit, sir a very palpible hit.");
             }
             }
 }
     }
     if(dir === "up"){
-        //console.log("player",player.yPos)
-        //console.log("enemy",enemy.yPos)
         for(let x of objects){
             if ((x.yPos< player.yPos)&&(x.xPos-80 < player.xPos )){
                 if((x.xPos>player.xPos)){
                 x.casting = "disarmed";
-                //console.log("hit, sir a very palpible hit.");
             }
             }
 }
@@ -409,6 +444,7 @@ function cast_fire(dir){
 
 }
 function move_spells(){
+    // moving the fire spells and eliminating any enemy that it toutches 
     for(let fire of fire_spells){
         if(fire.direction == "left"){
             fire.xPos -= 10;
@@ -453,18 +489,21 @@ function move_spells(){
     }
 }
 function defend_player(){
-    playerImage.src = "Wizard_Pack/Attack1.png";
+    // casts the defence spell
+    playerImageCur = 9;
     player.frameY = 0;
     player.frameX = (player.frameX + 231)%1848;
 }
 
 function move_enemy(){
+    // moving the enemy that casts the disarms
     for(let item of objects){
         if(item.type === "enemy"){
             let time = Date.now();
             
             if (time%10 === 0){
-                item.direction = Math.floor(Math.random() * 4);
+                // moving the enemy in a random direction when time % 10 = 0 
+                item.direction = Math.floor(Math.random() * 4);// learned about Math.random and Math.floor at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
             }
             if((item.direction === 0)&&(item.xPos > 0)){
                 item.xPos -= 1;
@@ -482,9 +521,11 @@ function move_enemy(){
     }
 }
 function check_if_hit(){
+    // only alowing enemy to cast if you are not casting a defend spell
     if(!(player.casting==="defend")){
         enemy_cast();
     }
+    // if disarmed calculate how long it will take to be rearmed again and if that time is 0 that return casting to normal
     for(let x of objects){
         if (x.casting === "disarmed"){
             if(x.time_until_rearm===0){
@@ -492,13 +533,13 @@ function check_if_hit(){
             x.time_until_rearm = 180;
         }
             x.time_until_rearm -= 1;
-            //console.log(player.time_until_rearm);
             context.drawImage(disarmImage,0,0,10,10, x.xPos, x.yPos-10,30,30);
         }
     }
 
 }
 function enemy_cast(){
+    // controling enemys casting disarmes
     for (let item of objects) {
         if(item.type === "enemy")
         if(!(item.casting === "disarmed")){
@@ -506,7 +547,8 @@ function enemy_cast(){
             if((item.direction === 0)&&(player.xPos<item.xPos)){
                 context.drawImage(disarmImage,0,0,10,10, item.xPos-20, item.yPos-10,30,30);
                 context.drawImage(disarmImage,0,0,10,10, item.xPos-40, item.yPos-10,30,30);
-                playerImage.src = "Wizard_Pack/Hit.png";
+                //playerImage.src = "Wizard_Pack/Hit.png";
+                playerImageCur = 10;
                 player.casting = "disarmed";
                 player.frameY = 0;
                 player.frameX = (player.frameX + 231)%1848;
@@ -516,7 +558,8 @@ function enemy_cast(){
             }
             else if((item.direction === 1)&&(player.xPos>item.xPos)){
                 context.drawImage(disarmImage,0,0,10,10, item.xPos, item.yPos-10,30,30);
-                playerImage.src = "Wizard_Pack/Hit_Right.png";
+                //playerImage.src = "Wizard_Pack/Hit_Right.png";
+                playerImageCur = 11;
                 player.frameY = 0;
                 player.casting = "disarmed";
                 player.frameX = (player.frameX + 231)%1848;
@@ -526,11 +569,11 @@ function enemy_cast(){
             }
 
         }
-        // new
         else if((player.xPos < item.xPos)&&(player.xPos > item.xPos-100)){
             if((item.direction === 2)&&(player.yPos>item.yPos)){
                 context.drawImage(disarmImage,0,0,10,10, item.xPos, item.yPos-10,30,30);
-                playerImage.src = "Wizard_Pack/Hit_down.png";
+                //playerImage.src = "Wizard_Pack/Hit_down.png";
+                playerImageCur = 13
                 player.frameX = 0;
                 player.casting = "disarmed";
                 player.frameY = (player.frameY + 231)%1848;
@@ -541,7 +584,8 @@ function enemy_cast(){
             }
             else if((item.direction === 3)&&(player.yPos< item.yPos)){
                 context.drawImage(disarmImage,0,0,10,10, item.xPos, item.yPos-10,30,30);
-                playerImage.src = "Wizard_Pack/Hit_up.png";
+                //playerImage.src = "Wizard_Pack/Hit_up.png";
+                playerImageCur = 12;
                 player.frameX = 0;
                 player.casting = "disarmed";
                 player.frameY = (player.frameY + 231)%1848;
@@ -556,6 +600,7 @@ function enemy_cast(){
 }
 
 function move_chasers(){
+    // move the chasers in such a way that they are chasing you
     for(let x of objects){
         if(x.type === "chaser"){
             if (player.xPos+55 < x.xPos){
@@ -570,21 +615,34 @@ function move_chasers(){
             else if (player.yPos > x.yPos){
                 x.yPos += 1;
             }
-            else{
+            else if(frames_since_start > 90){
                 game_over= true;
-                console.log(x.xPos, player.xPos);
-                console.log(x.yPos, player.yPos);
             }
         }
     }
 }
 function check_level_up(){
+    // controling what level you are on
     if((objects.length === 1)||(testLevel != 0)){
         if(level === 1){
-            populate_objects(1,0)
+            display_level.innerHTML = "1";
+            populate_objects(1,0);
         }
         else if((level === 2)||(testLevel==2)){
-            populate_objects(1,1)
+            display_level.innerHTML = "2";
+            populate_objects(2,0)
+        }
+        else if((level === 3)||(testLevel==3)){
+            display_level.innerHTML = "3";
+            populate_objects(2,1)
+        }
+        else if((level === 4)||(testLevel==4)){
+            display_level.innerHTML = "4";
+            populate_objects(2,2)
+        }
+        else if((level === 4)||(testLevel==4)){
+            display_level.innerHTML = "4";
+            populate_objects(4,2)
         }
         else{
             context.fillStyle ="red"
@@ -593,9 +651,12 @@ function check_level_up(){
         }
         level+=1;
         testLevel = 0;
+        fire_spells = [];
+        frames_since_start = 0;
     }
 }
 function populate_objects(num_chasers,num_enemys){
+    // spawn all the enemys needed for a level
     for (let i = 0; i < num_chasers; i += 1) {
                 let chaser = {
                     type:"chaser",
@@ -628,4 +689,49 @@ function populate_objects(num_chasers,num_enemys){
     };
     objects.push(enemy);
 }
+}
+function display_player(){
+    // displays the player in its different states
+    if(playerImageCur === 1){
+        context.drawImage(playerImageRight,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 2){
+        context.drawImage(playerImageLeft,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 3){
+        context.drawImage(playerImageUp,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 4){
+        context.drawImage(playerImageDown,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 5){
+        context.drawImage(playerImageAttackLeft,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 6){
+        context.drawImage(playerImageAttackRight,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 7){
+        context.drawImage(playerImageAttackUp,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 8){
+        context.drawImage(playerImageAttackDown,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 9){
+        context.drawImage(playerImageDefend,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 10){
+        context.drawImage(playerImageHitLeft,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 11){
+        context.drawImage(playerImageHitRight,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 12){
+        context.drawImage(playerImageHitUp,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else if(playerImageCur === 13){
+        context.drawImage(playerImageHitDown,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
+    else{ 
+        context.drawImage(playerImage,player.frameX,player.frameY,200,200, player.xPos, player.yPos,150,150);
+    }
 }
